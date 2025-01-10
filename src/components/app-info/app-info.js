4aed9b24f839de4d662be67a982b15d95ae5875c
Maks <savcukmaks68@gmail.com>
2025-01-10 14:41:40 +0200

    html on react

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css'
+
+const AppInfo = () => {
+    return(    
+        <div className="app-info">
+            <h1>Облік співробітників в компанії X</h1>
+            <h2>Загальна кількість: </h2>
+            <h2>Премію отримають: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
